7d35971ff342cf7a6a79c65ca123fd50f0bd93b0
SatriaBelva <satriabelvanararya@gmail.com>
2025-03-29 14:26:38 +0700

    Cool Form

--- streamlit/main.py
+++ streamlit/main.py
@@ -178,3 +178,24 @@ st.button(
 )
 
 
+# Contoh Yang Benar 
+st.markdown("<h1 style='text-align : center;'>User Registration</h1>", unsafe_allow_html=True)
+with st.form(key='form2', clear_on_submit=False, enter_to_submit=False) :
+    col1, col2, col3 = st.columns(spec=3)
+    firstname = col1.text_input(label='Firstname')
+    surname = col2.text_input(label='Surname')
+    lastname = col3.text_input(label='Lastname')
+    username = st.text_input(label='Username', help='username harus unik')
+    st.date_input(label='Silahkan Pilih Tanggal Lahir Anda',help='Silahkan Pilih Tanggal Lahir Anda',format='DD/MM/YYYY',max_value=datetime.date.today(),min_value=datetime.date(1925, 6, 23), value=datetime.date(2002, 8, 31) )
+    password1 = st.text_input(label='Password', type='password', help='password boleh berbeda')
+    password2 = st.text_input(label='Confirm Password', type='password')
+    submitButton = st.form_submit_button(label='Submit', use_container_width=True, type='secondary' )
+    if submitButton :
+        if firstname == "" or surname == "" or lastname == "" :
+            st.warning(body="Please Fill in Your Name", icon='⚠️')
+        elif username == "" :
+            st.error(body="Goblog", icon='🧠')
+        elif password1 != password2 :
+            st.warning(body="Password Tidak Sama", icon='⚠️')
+        else : 
+            st.success(body='Akun Berhasil Dibuat', icon='🔥')
